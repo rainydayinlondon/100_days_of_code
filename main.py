bafdcde43fc4e4defa4571e36c1d8b75fa2f4289
rainydayinlondon <rainydyinlondon@gmail.com>
2022-03-25 12:05:41 +0300

    Add files via upload
    Turtle Module  / for and while loop

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-money_machine=MoneyMachine()
-coffee_maker=CoffeeMaker()
-menu =Menu()
-is_on=True
+from turtle import Turtle,Screen  # turtle.py scriptinden Turtle ve Screen classlarını getir
+#from turtle import *
+
+ninja=Turtle()
+
+ninja.shape('turtle')
+ninja.color('red')
+ninja.shapesize(2, 2, 2)
 
 
-coffee_maker.report()
-money_machine.report()
-
-while is_on:
-    options=menu.get_items()
-    choice=input(f"What would you like ({options})")
-    if choice=="off":
-        is_on=False
-    elif choice=="report":
-        coffee_maker.report()
-        money_machine.report()
-    else:
-        drink=menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink):
-            if money_machine.make_payment(drink.cost):
-                coffee_maker.make_coffee(drink)
+# for i in range(4):
+#     ninja.forward(100)
+#     ninja.right(90)
 
 
+i=1
+while i<5:
+    ninja.forward(100)
+    ninja.right(90)
+    i=i+1
+
+
+
+
+# ninja.forward(100)
+# ninja.right(90)
+# ninja.forward(100)
+# ninja.right(90)
+# ninja.forward(100)
+
+screen=Screen()
+screen.exitonclick()
 
 
